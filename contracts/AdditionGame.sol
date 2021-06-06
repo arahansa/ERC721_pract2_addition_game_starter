0fb99d7b58ba7428c308dc54fc8f005456bb402d
arahansa <arahansa@naver.com>
2021-06-06 17:42:04 +0900

    배포해본 버전

--- contracts/AdditionGame.sol
+++ contracts/AdditionGame.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.2;
 
 contract AdditionGame {
     address public owner;
@@ -6,15 +6,15 @@ contract AdditionGame {
     constructor() public {
         owner = msg.sender;
     }
-    
+
     function getBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    function deposit() public payable {  
+    function deposit() public payable {
         require(msg.sender == owner);
-    }   
-  
+    }
+
     function transfer(uint _value) public returns (bool) {
         require(getBalance() >= _value);
         msg.sender.transfer(_value);
